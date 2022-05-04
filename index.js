0d31dc7c7cb040953880f3ee6e3153076f8c9c1b
mirajul alam <alifmirajulalam@gmail.comm>
2022-05-04 22:52:30 +0600

    get delete methods added

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 7000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 // middle ware
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eu99l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -29,6 +29,27 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const products = await fruitsCollection.findOne(query)
             res.send(products)
+        })
+        // update quantity
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.UpdateQuantity
+                }
+            };
+            const result = await fruitsCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        });
+        // delete product
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await fruitsCollection.deleteOne(query)
+            res.send(result)
         })
     }
     finally { }
